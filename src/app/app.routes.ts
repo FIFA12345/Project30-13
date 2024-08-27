48a8455cda2b65380808ec27e17932feeb3cbe84
Teerayut Thongpan <Teerayutthongpan@gmail.com>
2024-08-27 09:56:21 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { Component } from '@angular/core';
+export const routes: Routes = [
+{
+    path: 'home',
+    component: HomeComponent,
+},
+{
+    path: 'about',
+    component: AboutComponent,
+},
+{
+    path: 'login',
+    component: LoginComponent,
+},
+{
+    path: 'register',
+    component: RegisterComponent,
+}
+];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule,FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  //FormsGroup
+  loginForm!: FormGroup;
+  //สร้างตัวแปรไว้เช็คว่า Submit form หรือยัง
+  submitted = false;
+  //ตัวแปรสำหรับผูกกับ form
+  userLogin = {
+    "email": "",
+    "password": "",
+  }
+  constructor(private formBuilder: FormBuilder) {
+
+   }
+   ngOnInit(){
+    this.loginForm = this.formBuilder.group({
+      "email": ['', [Validators.required,Validators.email]],
+      "password": ['',[Validators.required, Validators.minLength(6)]],  
+    })
+
+  }
+
+  //ปุ่ม login
+  submitlogin(){
+    this.submitted = true;
+    // ถ้าฟอร์มไม่ถูกต้อง (Invalid)
+    if (this.loginForm.invalid){
+      return
+    }else{
+      this.userLogin.email = this.loginForm.value.email
+      this.userLogin.password = this.loginForm.value.password
+      if(this.userLogin.email == "admin@gmail.com"
+      && this.userLogin.password == "123456"){
+        alert("เข้าสู่ระบบสำเร็จ")
+      }else{
+        alert("ไม่สามารถเข้าสู่ระบบได้ ")
+      }
+    }
+  }
+
+  // ปุ่ม reset form
+  resetForm(){
+    this.submitted = false;
+    this.loginForm.reset();
+  }   
+}
